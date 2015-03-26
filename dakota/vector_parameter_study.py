d860de48f9332f6443996838809d33cccd2ef55b
Mark Piper <mark.piper@colorado.edu>
2015-03-26 16:41:36 -0600

    Start the VectorParameterStudy write method

--- dakota/vector_parameter_study.py
+++ dakota/vector_parameter_study.py
@@ -15,3 +15,15 @@ class VectorParameterStudy(Dakota):
         self.final_point = []
         self.n_steps = 0
 
+    def write(self):
+        """Write a Dakota input file."""
+        indent = '  '
+        with open(self.input_file, 'w') as fp:
+            fp.write('# Dakota input file\n')
+            fp.write('environment\n')
+            fp.write('{0}{1}\n'.format(indent, 'tabular_data'))
+            fp.write('{0}{0}{1} = {2!r}\n'.format(indent,
+                                                  'tabular_data_file',
+                                                  self.data_file))
+            fp.write('\nmethod\n')
+            fp.write('{0}{1}\n'.format(indent, self.method))
