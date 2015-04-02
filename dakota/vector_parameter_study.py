9e6688eb9f8ff2fdc8ab16d08087146e0f54176a
Mark Piper <mark.piper@colorado.edu>
2015-04-02 16:36:06 -0600

    Subclass VectorParameterStudy from DakotaBase
    Also add the `method` helper function.

--- dakota/vector_parameter_study.py
+++ dakota/vector_parameter_study.py
@@ -1,24 +1,47 @@
 #! /usr/bin/env python
 """Implementation of a Dakota vector parameter study."""
 
-from .dakota import Dakota
+from .dakota_base import DakotaBase
 
 
-class VectorParameterStudy(Dakota):
-    """Set up a Dakota vector parameter study."""
+def method():
+    """A helper function to return a VectorParameterStudy object.
 
-    def __init__(self, input_file='dakota.in'):
+    Returns
+    -------
+    object
+      An instance of VectorParameterStudy.
+
+    Examples
+    --------
+    Call this function instead of the class constructor to obtain a
+    VectorParameterStudy instance.
+
+    >>> import dakota.vector_parameter_study as m
+    >>> v = m.model() # instead of v = m.VectorParameterStudy()
+    """
+    return VectorParameterStudy()
+
+class VectorParameterStudy(DakotaBase):
+    """Define parameters for a Dakota vector parameter study."""
+
+    def __init__(self):
         """Create a new Dakota vector parameter study."""
-        Dakota.__init__(self, input_file)
+        DakotaBase.__init__(self)
         self.method = 'vector_parameter_study'
         self.initial_point = [-0.3, 0.2]
         self.final_point = [1.1, 1.3]
         self.n_steps = 10
+        self.n_variables = len(self.initial_point)
         self.interface = 'direct'
         self.analysis_driver = 'rosenbrock'
+        self.n_responses = 1
+        self.autogenerate_descriptors()
 
     def method_block(self):
-        """Define the method block of a Dakota input file."""
+        """Define the method block of a Dakota input file for a vector
+        parameter study.
+        """
         s = 'method\n' \
             + '  {}\n'.format(self.method) \
             + '    final_point ='
@@ -29,7 +52,9 @@ class VectorParameterStudy(Dakota):
         return(s)
 
     def variables_block(self):
-        """Define the variables block of a Dakota input file."""
+        """Define the variables block of a Dakota input file for a vector
+        parameter study.
+        """
         s = 'variables\n' \
             + '  {0} = {1}\n'.format(self.variable_type, self.n_variables) \
             + '    initial_point ='
