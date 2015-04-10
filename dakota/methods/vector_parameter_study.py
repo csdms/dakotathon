ea05781d7d6891b3fadf93a15c2767278d3aabad
Mark Piper <mark.piper@colorado.edu>
2015-04-10 12:05:46 -0600

    Update VectorParameterStudy ctr to accept kwargs

--- dakota/methods/vector_parameter_study.py
+++ dakota/methods/vector_parameter_study.py
@@ -31,17 +31,17 @@ class VectorParameterStudy(DakotaBase):
 
     """Define parameters for a Dakota vector parameter study."""
 
-    def __init__(self):
+    def __init__(self, variable_descriptors=['x1', 'x2'],
+                 initial_point=[-0.3, 0.2], final_point=[1.1, 1.3], n_steps=10,
+                 response_descriptors=['y1'], **kwargs):
         """Create a new Dakota vector parameter study."""
-        DakotaBase.__init__(self)
+        DakotaBase.__init__(self, **kwargs)
         self.method = 'vector_parameter_study'
-        self.variable_descriptors = ['x1', 'x2']
-        self.initial_point = [-0.3, 0.2]
-        self.final_point = [1.1, 1.3]
-        self.n_steps = 10
-        self.interface = 'direct'
-        self.analysis_driver = 'rosenbrock'
-        self.response_descriptors = ['y1']
+        self.variable_descriptors = variable_descriptors
+        self.initial_point = initial_point
+        self.final_point = final_point
+        self.n_steps = n_steps
+        self.response_descriptors = response_descriptors
 
     def method_block(self):
         """Define a vector parameter study method block for a Dakota input file.
