cb0dec1ceae59bd54dad8ce22a8dac3dff9b05b2
Mark Piper <mark.piper@colorado.edu>
2015-04-13 11:56:14 -0600

    Update HydroTrend unit tests

--- dakota/tests/test_plugin_hydrotrend.py
+++ dakota/tests/test_plugin_hydrotrend.py
@@ -7,24 +7,33 @@
 #
 # Mark Piper (mark.piper@colorado.edu)
 
+import os
+import filecmp
 from nose.tools import *
 from dakota.plugins.hydrotrend import HydroTrend
+from dakota.utils import get_configuration
 from . import start_dir, data_dir
 
 
 # Global variables -----------------------------------------------------
+
+config_file = os.path.join(data_dir, 'config.yaml')
+params_file = os.path.join(data_dir, 'vector_parameter_study_params.in')
+known_results_file = os.path.join(data_dir, 'results.out')
+results_file = 'results.out'
 
 # Fixtures -------------------------------------------------------------
 
 def setup_module():
     """Called before any tests are performed."""
     print('\n*** ' + __name__)
-    global h
-    h = HydroTrend()
+    global config
+    config = get_configuration(params_file)
 
 def setup():
     """Called at start of any test using it @with_setup()"""
-    pass
+    global h
+    h = HydroTrend()
 
 def teardown():
     """Called at end of any test using it @with_setup()"""
@@ -32,16 +41,25 @@ def teardown():
 
 def teardown_module():
     """Called after all tests have completed."""
-    pass
+    if os.path.exists(results_file): os.remove(results_file)
 
 # Tests ----------------------------------------------------------------
 
 @raises(TypeError)
+@with_setup(setup, teardown)
 def test_load_zero_arguments():
     """Tests load() when no argument is passed."""
     r = h.load()
 
+@with_setup(setup, teardown)
 def test_load_does_not_exist():
     """Tests load() when a nonexistent output file is defined."""
     r = h.load('vfnqeubnuen.f')
     assert_is_none(r)
+
+@with_setup(setup, teardown)
+def test_write():
+    """Test the write method output versus a known results file."""
+    h.output_values = [1.0, 2.0]
+    labels = h.write(params_file, results_file)
+    assert_true(filecmp.cmp(known_results_file, results_file))
