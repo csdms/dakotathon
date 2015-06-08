bff788f00ccb7369455b5c50ac41095a08c330e7
Mark Piper <mark.piper@colorado.edu>
2015-06-08 10:06:04 -0600

    Introduce csdms namespace

--- setup.py
+++ setup.py
@@ -2,19 +2,22 @@
 from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
-from dakota import __version__, plugin_script
+from csdms.dakota import __version__, plugin_script
 
-setup(name='dakota',
+package_name = 'csdms.dakota'
+
+setup(name=package_name,
       version=__version__,
       author='Mark Piper',
       author_email='mark.piper@colorado.edu',
       license='MIT',
-      description='BMI for Dakota',
+      description='Python API for Dakota',
       long_description=open('README.md').read(),
+      namespace_packages=['csdms'], 
       packages=find_packages(exclude=['*.tests']),
       entry_points={
           'console_scripts': [
-              plugin_script + ' = dakota.run_plugin:main'
+              plugin_script + ' = ' + package_name + '.run_plugin:main'
           ]
       }
 )
