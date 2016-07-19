108cfa150de4b1ed04010c5b487817f732058db3
Mark Piper <mark.piper@colorado.edu>
2016-07-19 10:35:10 -0600

    Remove variables information
    I'm stripping out information that belongs in the 'variables' section
    of a Dakota input file and placing it in a new subpackage. The result
    looks more like how a vector parameter study is defined in the
    [Dakota docs](https://dakota.sandia.gov//sites/default/files/docs/6.4/html-ref/method-vector_parameter_study.html).

--- csdms/dakota/methods/vector_parameter_study.py
+++ csdms/dakota/methods/vector_parameter_study.py
@@ -12,20 +12,13 @@ class VectorParameterStudy(MethodsBase):
     """Define parameters for a Dakota vector parameter study."""
 
     def __init__(self,
-                 variables=('x1', 'x2'),
-                 initial_point=(-0.3, 0.2),
                  final_point=(1.1, 1.3),
                  n_steps=10,
-                 responses=('y1',),
                  **kwargs):
         """Create a new Dakota vector parameter study.
 
         Parameters
         ----------
-        variables, responses : array_like of str
-          Names used for input and output variables.
-        initial_point : array_like of float
-          Start point for the parameter study.
         final_point : array_like of float
           End point for the parameter study.
         n_steps : int
@@ -40,30 +33,8 @@ class VectorParameterStudy(MethodsBase):
         """
         MethodsBase.__init__(self, **kwargs)
         self.method = self.__module__.rsplit('.')[-1]
-        self.variables = variables
-        self._initial_point = initial_point
         self._final_point = final_point
         self.n_steps = n_steps
-        self.responses = responses
-
-    @property
-    def initial_point(self):
-        """Start points used by study variables."""
-        return self._initial_point
-
-    @initial_point.setter
-    def initial_point(self, value):
-        """Set start points used by study variables.
-
-        Parameters
-        ----------
-        value : list or tuple of numbers
-          The new initial points.
-
-        """
-        if not isinstance(value, (tuple, list)):
-            raise TypeError("Initial points must be a tuple or a list")
-        self._initial_point = value
 
     @property
     def final_point(self):
@@ -93,31 +64,10 @@ class VectorParameterStudy(MethodsBase):
 
         """
         s = 'method\n' \
-            + '  {}\n'.format(self.method) \
+            + '  {}\n'.format(self.name) \
             + '    final_point ='
         for pt in self.final_point:
             s += ' {}'.format(pt)
         s += '\n' \
             + '    num_steps = {}\n\n'.format(self.n_steps)
-        return(s)
-
-    def variables_block(self):
-        """Define a vector parameter study variables block for a Dakota input file.
-
-        See Also
-        --------
-        csdms.dakota.methods.base.MethodsBase.variables_block
-
-        """
-        s = 'variables\n' \
-            + '  {0} = {1}\n'.format(self.variable_type,
-                                     len(self.variables)) \
-            + '    initial_point ='
-        for pt in self.initial_point:
-            s += ' {}'.format(pt)
-        s += '\n' \
-             + '    descriptors ='
-        for vd in self.variables:
-            s += ' {!r}'.format(vd)
-        s += '\n\n'
         return(s)
