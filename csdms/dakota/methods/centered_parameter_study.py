944060feb84ad799c3014938201bd83d854f17f1
Mark Piper <mark.piper@colorado.edu>
2016-07-19 10:58:59 -0600

    Remove information used by variables block
    This information will be moved to code in the new variables
    subpackage.

--- csdms/dakota/methods/centered_parameter_study.py
+++ csdms/dakota/methods/centered_parameter_study.py
@@ -12,20 +12,13 @@ class CenteredParameterStudy(MethodsBase):
     """Define parameters for a Dakota centered parameter study."""
 
     def __init__(self,
-                 variables=('x1', 'x2'),
-                 initial_point=(0.0, 0.0),
                  steps_per_variable=(5, 4),
                  step_vector=(0.4, 0.5),
-                 responses=('y1',),
                  **kwargs):
         """Create a new Dakota centered parameter study.
 
         Parameters
         ----------
-        variables, responses : array_like of str
-          Names used for input and output variables.
-        initial_point : array_like of float
-          Start point (the center) for the parameter study.
         steps_per_variable : array_like of int
           Number of steps to take in each direction.
         steps_vector : array_like of float
@@ -40,30 +33,8 @@ class CenteredParameterStudy(MethodsBase):
         """
         MethodsBase.__init__(self, **kwargs)
         self.method = self.__module__.rsplit('.')[-1]
-        self.variables = variables
-        self._initial_point = initial_point
         self._steps_per_variable = steps_per_variable
         self._step_vector = step_vector
-        self.responses = responses
-
-    @property
-    def initial_point(self):
-        """Start points used by study variables."""
-        return self._initial_point
-
-    @initial_point.setter
-    def initial_point(self, value):
-        """Set start points used by study variables.
-
-        Parameters
-        ----------
-        value : list or tuple of numbers
-          The new initial points.
-
-        """
-        if not isinstance(value, (tuple, list)):
-            raise TypeError("Initial points must be a tuple or a list")
-        self._initial_point = value
 
     @property
     def steps_per_variable(self):
@@ -112,7 +83,7 @@ class CenteredParameterStudy(MethodsBase):
 
         """
         s = 'method\n' \
-            + '  {}\n'.format(self.method) \
+            + '  {}\n'.format(self.name) \
             + '    steps_per_variable ='
         for step in self.steps_per_variable:
             s += ' {}'.format(step)
@@ -120,26 +91,5 @@ class CenteredParameterStudy(MethodsBase):
             + '    step_vector ='
         for step in self.step_vector:
             s += ' {}'.format(step)
-        s += '\n\n'
-        return(s)
-
-    def variables_block(self):
-        """Define a centered parameter study variables block.
-
-        See Also
-        --------
-        csdms.dakota.methods.base.MethodsBase.variables_block
-
-        """
-        s = 'variables\n' \
-            + '  {0} = {1}\n'.format(self.variable_type,
-                                     len(self.variables)) \
-            + '    initial_point ='
-        for pt in self.initial_point:
-            s += ' {}'.format(pt)
-        s += '\n' \
-             + '    descriptors ='
-        for vd in self.variables:
-            s += ' {!r}'.format(vd)
         s += '\n\n'
         return(s)
