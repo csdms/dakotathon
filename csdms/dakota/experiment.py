c667ca9016fafb3a42bd185c14ca14d9c042cf79
Mark Piper <mark.piper@colorado.edu>
2016-07-21 16:11:00 -0600

    Convert attributes to properties
    This allows me to provide type checking when setting.

--- csdms/dakota/experiment.py
+++ csdms/dakota/experiment.py
@@ -20,7 +20,63 @@ class Experiment(object):
                         'interface', 'responses')
         for section in self._blocks:
             cls = self._import(section, eval(section), **kwargs)
-            setattr(self, section, cls)
+            attr = '_' + section
+            setattr(self, attr, cls)
+
+    @property
+    def environment(self):
+        return self._environment
+
+    @environment.setter
+    def environment(self, value):
+        supr = self._environment.__class__.__bases__[0]
+        if not isinstance(value, supr):
+            raise TypeError("Must be a subclass of " + str(supr))
+        self._environment = value
+
+    @property
+    def method(self):
+        return self._method
+
+    @method.setter
+    def method(self, value):
+        supr = self._method.__class__.__bases__[0]
+        if not isinstance(value, supr):
+            raise TypeError("Must be a subclass of " + str(supr))
+        self._method = value
+
+    @property
+    def variables(self):
+        return self._variables
+
+    @variables.setter
+    def variables(self, value):
+        supr = self._variables.__class__.__bases__[0]
+        if not isinstance(value, supr):
+            raise TypeError("Must be a subclass of " + str(supr))
+        self._variables = value
+
+    @property
+    def interface(self):
+        return self._interface
+
+    @interface.setter
+    def interface(self, value):
+        supr = self._interface.__class__.__bases__[0]
+        if not isinstance(value, supr):
+            raise TypeError("Must be a subclass of " + str(supr))
+        self._interface = value
+
+    @property
+    def responses(self):
+        return self._responses
+
+    @responses.setter
+    def responses(self, value):
+        supr = self._responses.__class__.__bases__[0]
+        if not isinstance(value, supr):
+            raise TypeError("Must be a subclass of " + str(supr))
+        self._responses = value
 
     def _get_subpackage_namespace(self, subpackage):
         return os.path.splitext(self.__module__)[0] + '.' + subpackage
