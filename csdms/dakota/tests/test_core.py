2c8509af24f922ead5343832c3424af81dc6a4af
Mark Piper <mark.piper@colorado.edu>
2016-07-23 14:16:47 -0600

    Complete tests for core module

--- csdms/dakota/tests/test_core.py
+++ csdms/dakota/tests/test_core.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Tests for dakota.core module.
+# Tests for the csdms.dakota.core module.
 #
 # Call with:
 #   $ nosetests -sv
@@ -10,7 +10,8 @@
 import os
 import filecmp
 from subprocess import CalledProcessError
-from nose.tools import assert_is_instance, assert_true, raises
+from nose.tools import (raises, assert_is_instance, assert_true,
+                        assert_is_none, assert_equal)
 from csdms.dakota.core import Dakota
 from csdms.dakota.utils import is_dakota_installed
 from . import start_dir, data_dir
@@ -24,7 +25,8 @@ input_file, \
     restart_file = ['dakota.' + ext for ext in ('in', 'out', 'dat', 'rst')]
 alt_input_file = 'alt.in'
 known_file = os.path.join(data_dir, 'default_vps_dakota.in')
-config_file = os.path.join(data_dir, 'default_vps_config.yaml')
+known_config_file = os.path.join(data_dir, 'default_vps_config.yaml')
+default_config_file = os.path.join(os.getcwd(), 'config.yaml')
 tmp_files = [input_file, alt_input_file, output_file, data_file,
              restart_file, 'config.yaml']
 
@@ -32,12 +34,14 @@ tmp_files = [input_file, alt_input_file, output_file, data_file,
 
 
 def setup_module():
-    """Called before any tests are performed."""
+    """Fixture called before any tests are performed."""
     print('\n*** ' + __name__)
+    global d
+    d = Dakota()
 
 
 def teardown_module():
-    """Called after all tests have completed."""
+    """Fixture called after all tests have completed."""
     for f in tmp_files:
         if os.path.exists(f):
             os.remove(f)
@@ -47,79 +51,141 @@ def teardown_module():
 
 def test_init_no_parameters():
     """Test constructor with no parameters."""
-    d = Dakota()
-    assert_is_instance(d, Dakota)
+    k = Dakota()
+    assert_is_instance(k, Dakota)
 
 
 def test_init_method_parameter():
     """Test constructor with method parameter."""
-    d = Dakota(method='vector_parameter_study')
-    assert_is_instance(d, Dakota)
+    k = Dakota(method='vector_parameter_study')
+    assert_is_instance(k, Dakota)
 
 
 @raises(ImportError)
 def test_init_method_parameter_unknown_module():
     """Test constructor with method parameter fails with unknown module."""
-    d = Dakota(method='foo')
+    k = Dakota(method='__foo$')
 
 
 def test_init_from_file_like1():
     """Test creating an instance from a config file."""
-    d = Dakota.from_file_like(config_file)
-    assert_is_instance(d, Dakota)
+    k = Dakota.from_file_like(known_config_file)
+    assert_is_instance(k, Dakota)
 
 
 def test_init_from_file_like2():
     """Test creating an instance from an open config file object."""
-    with open(config_file, 'r') as fp:
-        d = Dakota.from_file_like(fp)
-    assert_is_instance(d, Dakota)
+    with open(known_config_file, 'r') as fp:
+        k = Dakota.from_file_like(fp)
+    assert_is_instance(k, Dakota)
+
+
+def test_get_run_directory():
+    """Test getting the run_directory property."""
+    assert_equal(d.run_directory, os.getcwd())
+
+
+def test_set_run_directory():
+    """Test setting the run_directory property."""
+    k = Dakota()
+    run_dir = '/foo/bar'
+    k.run_directory = run_dir
+    assert_equal(k.run_directory, run_dir)
+
+
+def test_get_configuration_file():
+    """Test getting the configuration_file property."""
+    assert_equal(d.configuration_file, default_config_file)
+
+
+def test_set_configuration_file():
+    """Test setting the configuration_file property."""
+    k = Dakota()
+    k.configuration_file = known_config_file
+    assert_equal(k.configuration_file, known_config_file)
+
+
+def test_get_template_file():
+    """Test getting the template_file property."""
+    assert_is_none(d.template_file)
+
+
+def test_set_template_file():
+    """Test setting the template_file property."""
+    k = Dakota()
+    template_file = 'foo.tmpl'
+    k.template_file = template_file
+    assert_equal(os.path.basename(k.template_file), template_file)
+
+
+def test_get_auxiliary_files():
+    """Test getting the auxiliary_files property."""
+    assert_equal(d.auxiliary_files, tuple())
+
+
+def test_set_auxiliary_files():
+    """Test setting the auxiliary_files property."""
+    k = Dakota()
+    for auxiliary_file in ['foo.in', ['foo.in'], ('foo.in',)]:
+        k.auxiliary_files = auxiliary_file
+        if type(auxiliary_file) is not str:
+            auxiliary_file = auxiliary_file[0]
+        pathified_auxiliary_file = os.path.abspath(auxiliary_file)
+        assert_equal(k.auxiliary_files, (pathified_auxiliary_file,))
+
+
+@raises(TypeError)
+def test_set_auxiliary_files_fails_if_scalar():
+    """Test that auxiliary_files fails with a non-string scalar."""
+    k = Dakota()
+    auxiliary_file = 42
+    k.auxiliary_files = auxiliary_file
 
 
 def test_write_configuration_file():
     """Test write_configuration_file produces config file."""
-    d = Dakota(method='vector_parameter_study')
-    d.write_configuration_file()
-    assert_true(os.path.exists(d.method.configuration_file))
+    k = Dakota(method='vector_parameter_study')
+    k.write_configuration_file()
+    assert_true(os.path.exists(k.configuration_file))
 
 
 def test_write_input_file_with_method_default_name():
     """Test write_input_file works when instanced with method."""
-    d = Dakota(method='vector_parameter_study')
-    d.write_input_file()
-    assert_true(os.path.exists(d.input_file))
+    k = Dakota(method='vector_parameter_study')
+    k.write_input_file()
+    assert_true(os.path.exists(k.input_file))
 
 
 def test_write_input_file_with_method_new_name():
     """Test write_input_file works when instanced with method and new name."""
-    d = Dakota(method='vector_parameter_study')
-    d.write_input_file(input_file=alt_input_file)
-    assert_true(os.path.exists(d.input_file))
+    k = Dakota(method='vector_parameter_study')
+    k.write_input_file(input_file=alt_input_file)
+    assert_true(os.path.exists(k.input_file))
 
 
 def test_input_file_contents():
     """Test write_input_file results versus a known input file."""
-    d = Dakota(method='vector_parameter_study')
-    d.write_input_file()
+    k = Dakota(method='vector_parameter_study')
+    k.write_input_file()
     assert_true(filecmp.cmp(known_file, input_file))
 
 
 def test_setup():
-    d = Dakota(method='vector_parameter_study')
-    d.write_configuration_file()
-    d.write_input_file()
-    assert_true(os.path.exists(d.method.configuration_file))
+    k = Dakota(method='vector_parameter_study')
+    k.write_configuration_file()
+    k.write_input_file()
+    assert_true(os.path.exists(k.configuration_file))
     assert_true(filecmp.cmp(known_file, input_file))
 
 
 def test_default_run_with_input_file():
     """Test default object run method with input file."""
     if is_dakota_installed():
-        d = Dakota()
-        d.write_input_file()
-        d.run()
-        assert_true(os.path.exists(d.output_file))
-        assert_true(os.path.exists(d.method.data_file))
+        k = Dakota()
+        k.write_input_file()
+        k.run()
+        assert_true(os.path.exists(k.output_file))
+        assert_true(os.path.exists(k.environment.data_file))
 
 
 def test_default_run_without_input_file():
@@ -128,7 +194,7 @@ def test_default_run_without_input_file():
         if os.path.exists(input_file):
             os.remove(input_file)
         try:
-            d = Dakota()
-            d.run()
+            k = Dakota()
+            k.run()
         except CalledProcessError:
             pass
