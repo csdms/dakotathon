853251c2d0d0a03c8c50d2d09cb758648c6b2694
Mark Piper <mark.piper@colorado.edu>
2016-07-26 11:42:13 -0600

    Make 'method' attribute into a property

--- csdms/dakota/method/base.py
+++ csdms/dakota/method/base.py
@@ -22,7 +22,26 @@ class MethodBase(object):
           The name of the analysis method; e.g., 'vector_parameter_study'.
 
         """
-        self.method = method
+        self._method = method
+
+    @property
+    def method(self):
+        """The analysis method used in the experiment."""
+        return self._method
+
+    @method.setter
+    def method(self, value):
+        """Set the analysis method used in the experiment.
+
+        Parameters
+        ----------
+        value : str
+          The new method type.
+
+        """
+        if not isinstance(value, str):
+            raise TypeError("Method must be a str")
+        self._method = value
 
     def __str__(self):
         """Define the preamble of the Dakota input file method block."""
