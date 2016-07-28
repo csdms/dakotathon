5fc729bf46b0c91eed74a1d83c4b2c7fed8d51b0
Mark Piper <mark.piper@colorado.edu>
2016-07-28 15:56:10 -0600

    Create new ABC for UQ methods

--- csdms/dakota/method/base.py
+++ csdms/dakota/method/base.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-"""An abstract base class for all Dakota analysis methods."""
+"""Abstract base classes for Dakota analysis methods."""
 
 from abc import ABCMeta, abstractmethod
 import types
@@ -48,3 +48,43 @@ class MethodBase(object):
         s = 'method\n' \
             + '  {}'.format(self.method)
         return(s)
+
+
+class UncertaintyQuantificationBase(MethodBase):
+
+    """Describe features of uncertainty quantification methods."""
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self, basis_polynomial_family='extended', **kwargs):
+        """Create default method parameters.
+
+        Parameters
+        ----------
+        basis_polynomial_family: str, optional
+          The type of polynomial basis used in the expansion, either
+          'extended' (the default), 'askey', or 'wiener'.
+
+        """
+        self._basis_polynomial_family = basis_polynomial_family
+
+    @property
+    def basis_polynomial_family(self):
+        """The type of basis polynomials used by the method."""
+        return self._basis_polynomial_family
+
+    @basis_polynomial_family.setter
+    def basis_polynomial_family(self, value):
+        """Set the type of basis polynomials used by the method.
+
+        Parameters
+        ----------
+        value : str
+        The polynomial type.
+
+        """
+        if value not in ('extended', 'askey', 'wiener'):
+            msg = "Polynomial type must be 'extended', 'askey', or 'wiener'"
+            raise TypeError(msg)
+        self._basis_polynomial_family = value
---
+++ csdms/dakota/tests/test_method_base_uq.py
@@ -0,0 +1,49 @@
+"""Tests for csdms.dakota.method.base.UncertaintyQuantificationBase."""
+
+from nose.tools import raises, assert_true, assert_equal
+from csdms.dakota.method.base import UncertaintyQuantificationBase
+
+
+class Concrete(UncertaintyQuantificationBase):
+
+    """A subclass of UncertaintyQuantificationBase used for testing."""
+
+    def __init__(self, **kwargs):
+        UncertaintyQuantificationBase.__init__(self, **kwargs)
+
+
+def setup_module():
+    """Called before any tests are performed."""
+    print('\n*** ' + __name__)
+    global c
+    c = Concrete()
+
+
+def teardown_module():
+    """Called after all tests have completed."""
+    pass
+
+
+@raises(TypeError)
+def test_instantiate():
+    """Test whether UncertaintyQuantificationBase fails to instantiate."""
+    d = UncertaintyQuantificationBase()
+
+
+def test_get_basis_polynomial_family():
+    """Test getting the basis_polynomial_family property."""
+    assert_true(type(c.basis_polynomial_family) is str)
+
+
+def test_set_basis_polynomial_family():
+    """Test setting the basis_polynomial_family property."""
+    m = 'askey'
+    c.basis_polynomial_family = m
+    assert_equal(c.basis_polynomial_family, m)
+
+
+@raises(TypeError)
+def test_basis_polynomial_family_fails_if_unknown_type():
+    """Test that setting basis_polynomial_family to an unknown type fails."""
+    value = 'foobar'
+    c.basis_polynomial_family = value
