67be3f345e3ea5111e487fdca4c7f6a75df22847
Mark Piper <mark.piper@colorado.edu>
2016-07-29 11:25:25 -0600

    First pass at creating polynomial_chaos method
    This isn't a great place to commit, but I wanted to make a
    checkpoint because things are getting complicated.

---
+++ csdms/dakota/method/polynomial_chaos.py
@@ -0,0 +1,262 @@
+#! /usr/bin/env python
+"""Implementation of the Dakota polynomial chaos method."""
+
+from .base import UncertaintyQuantificationBase
+
+
+classname = 'PolynomialChaos'
+
+
+class PolynomialChaos(UncertaintyQuantificationBase):
+
+    """The Dakota polynomial chaos analysis method.
+
+    The coefficient estimation approach is required, but the only
+    approach currently implemented is *quadrature_order_sequence*,
+    which obtains coefficients of the expansion using multidimensional
+    integration by a tensor-product of Gaussian quadrature rules
+    specified with *quadrature_order*, and, optionally, with
+    *dimension_preference*. If *dimension_preference* is defined,
+    its highest value is set to the *quadrature_order*.
+
+    """
+
+    def __init__(self,
+                 coefficient_estimation_approach='quadrature_order_sequence',
+                 quadrature_order=2,
+                 dimension_preference=(),
+                 nested=False,
+                 probability_levels=(),
+                 response_levels=(),
+                 samples=10,
+                 sample_type='random',
+                 seed=None,
+                 variance_based_decomp=False,
+                 **kwargs):
+        """Create a new Dakota sampling study.
+
+        Parameters
+        ----------
+        coefficient_estimation_approach : str
+          Technique to obtain coefficients of expansion.
+        quadrature_order : int
+          The highest order of the polynomial basis.
+        dimension_preference : list or tuple of int, optional
+          A set of weights specifying the relative importance of each
+          uncertain variable (dimension).
+        nested : bool, optional
+          Set to enforce nested quadrature rules, if available (default is False).
+        probability_levels : list or tuple of float, optional
+         Specify probability levels at which to estimate the
+         corresponding response value.
+        samples: int
+          The number of randomly chosen values at which to execute a model.
+        sample_type: str
+          Technique for choosing samples, `random` or `lhs`.
+        seed: int, optional
+          The seed for the random number generator. If seed is
+          specified, a stochastic study will generate identical
+          results when repeated using the same seed value. If not
+          specified, a seed is randomly generated.
+
+        Examples
+        --------
+        Create a default instance of PolynomialChaos with:
+
+        >>> m = PolynomialChaos()
+
+        """
+        UncertaintyQuantificationBase.__init__(self, **kwargs)
+        self.method = self.__module__.rsplit('.')[-1]
+        self.coefficient_estimation_approach = coefficient_estimation_approach
+        self._quadrature_order = quadrature_order
+        self._dimension_preference = dimension_preference
+        self._nested = nested
+        self._probability_levels = probability_levels
+        self._samples = samples
+        self._sample_type = sample_type
+        self._seed = seed
+
+        if len(self.dimension_preference) > 0:
+            self.quadrature_order = max(self.dimension_preference)
+
+    @property
+    def quadrature_order(self):
+        """The highest order polynomial used by the method."""
+        return self._quadrature_order
+
+    @quadrature_order.setter
+    def quadrature_order(self, value):
+        """Set the highest order polynomial used by the method.
+
+        Parameters
+        ----------
+        value : int
+          The polynomial order.
+
+        """
+        if type(value) is not int:
+            raise TypeError("Quadrature order must be an int")
+        if len(self._dimension_preference) > 0:
+            self._quadrature_order = max(self._dimension_preference)
+        else:
+            self._quadrature_order = value
+
+    @property
+    def dimension_preference(self):
+        """Weights specifying the relative importance of each dimension."""
+        return self._dimension_preference
+
+    @dimension_preference.setter
+    def dimension_preference(self, value):
+        """Set weights specifying the relative importance of each dimension.
+
+        The highest value of dimension_preference is set as the
+        quadrature_order.
+
+        Parameters
+        ----------
+        value : tuple or list of int
+          The weights.
+
+        """
+        if not isinstance(value, (tuple, list)):
+            raise TypeError("Dimension preference must be a tuple or a list")
+        self._dimension_preference = value
+        self.quadrature_order = max(self.dimension_preference)
+
+    @property
+    def nested(self):
+        """Enforce use of nested quadrature rules."""
+        return self._nested
+
+    @nested.setter
+    def nested(self, value):
+        """Toggle use of nested quadrature rules.
+
+        Parameters
+        ----------
+        value : bool
+          True if nested.
+
+        """
+        if type(value) is not bool:
+            raise TypeError("Nested must be a bool")
+        self._nested = value
+
+    @property
+    def probability_levels(self):
+        """Probabilities at which to estimate response values."""
+        return self._probability_levels
+
+    @probability_levels.setter
+    def probability_levels(self, value):
+        """Set probabilities at which to estimate response values.
+
+        Parameters
+        ----------
+        value : tuple or list of int
+          The probability levels.
+
+        """
+        if not isinstance(value, (tuple, list)):
+            raise TypeError("Probability levels must be a tuple or a list")
+        self._probability_levels = value
+
+    @property
+    def samples(self):
+        """Number of samples in experiment."""
+        return self._samples
+
+    @samples.setter
+    def samples(self, value):
+        """Set number of samples used in experiment.
+
+        Parameters
+        ----------
+        value : int
+          The number of samples.
+
+        """
+        if type(value) is not int:
+            raise TypeError("Samples must be an int")
+        self._samples = value
+
+    @property
+    def sample_type(self):
+        """Sampling strategy."""
+        return self._sample_type
+
+    @sample_type.setter
+    def sample_type(self, value):
+        """Set sampling strategy used in experiment.
+
+        Parameters
+        ----------
+        value : str
+          The sampling strategy.
+
+        """
+        if ['random', 'lhs'].count(value) == 0:
+            raise TypeError("Sample type must be 'random' or 'lhs'")
+        self._sample_type = value
+
+    @property
+    def seed(self):
+        """Seed of the random number generator."""
+        return self._seed
+
+    @seed.setter
+    def seed(self, value):
+        """Set the seed of the random number generator.
+
+        Parameters
+        ----------
+        value : int
+          The random number generator seed.
+
+        """
+        if type(value) is not int:
+            raise TypeError("Seed must be an int")
+        self._seed = value
+
+    def __str__(self):
+        """Define the method block for a polynomial_chaos experiment.
+
+        Examples
+        --------
+        Display the method block created by a default instance of
+        PolynomialChaos:
+
+        >>> m = PolynomialChaos()
+        >>> print m
+
+        See Also
+        --------
+        csdms.dakota.method.base.UncertaintyQuantificationBase.__str__
+
+        """
+        s = UncertaintyQuantificationBase.__str__(self)
+        if self.coefficient_estimation_approach == 'quadrature_order_sequence':
+            s += '    quadrature_order = {}\n'.format(self.quadrature_order)
+            if len(self.dimension_preference) > 0:
+                s += '    dimension_preference ='
+                for item in self.dimension_preference:
+                    s += ' {}'.format(item)
+                s += '\n'
+            if self.nested:
+                s += '    nested\n'
+            else:
+                s += '    non_nested\n'
+        if len(self.probability_levels) > 0:
+            s += '    probability_levels =\n'
+            s += '     '
+            for item in self.probability_levels:
+                s += ' {}'.format(item)
+            s += '\n'
+        s += '    sample_type = {}\n'.format(self.sample_type) \
+            + '    samples = {}\n'.format(self.samples)
+        if self.seed is not None:
+            s += '    seed = {}\n'.format(self.seed)
+        s += '\n'
+        return(s)
---
+++ csdms/dakota/tests/test_method_polynomial_chaos.py
@@ -0,0 +1,191 @@
+"""Tests for the csdms.dakota.method.polynomial_chaos module."""
+
+from nose.tools import (raises, assert_is_instance, assert_true,
+                        assert_equal, assert_is_none)
+from csdms.dakota.method.polynomial_chaos import PolynomialChaos
+
+
+def setup_module():
+    """Called before any tests are performed."""
+    print('\n*** ' + __name__)
+    global x
+    x = PolynomialChaos()
+
+
+def teardown_module():
+    """Called after all tests have completed."""
+    pass
+
+
+def test_init_no_params():
+    """Test creating an instance with no parameters."""
+    x1 = PolynomialChaos()
+    assert_is_instance(x1, PolynomialChaos)
+
+
+def test_method_attr():
+    """Test the value of the method attribute."""
+    assert_equal(x.method, 'polynomial_chaos')
+
+
+def test_get_quadrature_order():
+    """Test getting the quadrature_order property."""
+    assert_true(type(x.quadrature_order) is int)
+
+
+def test_set_quadrature_order():
+    """Test setting the quadrature_order property."""
+    m = PolynomialChaos()
+    quadrature_order = 42
+    m.quadrature_order = quadrature_order
+    assert_equal(m.quadrature_order, quadrature_order)
+
+
+@raises(TypeError)
+def test_set_quadrature_order_fails_if_float():
+    """Test that the quadrature_order property fails with a float."""
+    m = PolynomialChaos()
+    quadrature_order = 42.0
+    m.quadrature_order = quadrature_order
+
+
+def test_get_dimension_preference():
+    """Test getting the dimension_preference property."""
+    assert_true(type(x.dimension_preference) is tuple)
+
+
+def test_set_dimension_preference():
+    """Test setting the dimension_preference property."""
+    m = PolynomialChaos()
+    for items in [[0,1], (0,1)]:
+        m.dimension_preference = items
+        assert_equal(m.dimension_preference, items)
+
+
+@raises(TypeError)
+def test_set_dimension_preference_fails_if_scalar():
+    """Test that the dimension_preference property fails with scalar."""
+    m = PolynomialChaos()
+    pt = 42
+    m.dimension_preference = pt
+
+
+def test_dimension_preference_sets_quadrature_order1():
+    """Test that dimension_preference sets quadrature order."""
+    m = PolynomialChaos()
+    m.dimension_preference = [3, 4, 5]
+    assert_equal(m.quadrature_order, max(m.dimension_preference))
+
+
+def test_dimension_preference_sets_quadrature_order2():
+    """Test that dimension_preference sets quadrature order."""
+    m = PolynomialChaos(dimension_preference=[3, 4, 5])
+    assert_equal(m.quadrature_order, max(m.dimension_preference))
+
+
+def test_dimension_preference_sets_quadrature_order3():
+    """Test that dimension_preference sets quadrature order."""
+    m = PolynomialChaos(dimension_preference=[3, 4, 5])
+    m.quadrature_order=42
+    assert_equal(m.quadrature_order, max(m.dimension_preference))
+
+
+def test_get_nested():
+    """Test getting the nested property."""
+    assert_true(type(x.nested) is bool)
+
+
+def test_set_nested():
+    """Test setting the nested property."""
+    m = PolynomialChaos()
+    nested = True
+    m.nested = nested
+    assert_equal(m.nested, nested)
+
+
+@raises(TypeError)
+def test_set_nested_fails_if_float():
+    """Test that the nested property fails with a float."""
+    m = PolynomialChaos()
+    nested = 42.0
+    m.nested = nested
+
+#---
+
+def test_get_samples():
+    """Test getting the samples property."""
+    assert_true(type(x.samples) is int)
+
+
+def test_set_samples():
+    """Test setting the samples property."""
+    m = PolynomialChaos()
+    samples = 42
+    m.samples = samples
+    assert_equal(m.samples, samples)
+
+
+@raises(TypeError)
+def test_set_samples_fails_if_float():
+    """Test that the samples property fails with a float."""
+    m = PolynomialChaos()
+    samples = 42.0
+    m.samples = samples
+
+
+def test_get_sample_type():
+    """Test getting the sample_type property."""
+    assert_true(x.sample_type == 'random' or x.sample_type == 'lhs')
+
+
+def test_set_sample_type():
+    """Test setting the sample_type property."""
+    m = PolynomialChaos()
+    sample_type = 'lhs'
+    m.sample_type = sample_type
+    assert_equal(m.sample_type, sample_type)
+
+
+@raises(TypeError)
+def test_set_sample_type_fails_if_not_lhs_or_random():
+    """Test that the sample_type property fails with unknown type."""
+    m = PolynomialChaos()
+    sample_type = 'mcmc'
+    m.sample_type = sample_type
+
+
+def test_get_seed():
+    """Test getting the seed property."""
+    if x.seed is not None:
+        assert_true(type(x.seed) is int)
+    else:
+        assert_is_none(x.seed)
+
+
+def test_set_seed():
+    """Test setting the seed property."""
+    m = PolynomialChaos()
+    seed = 42
+    m.seed = seed
+    assert_equal(m.seed, seed)
+
+
+@raises(TypeError)
+def test_set_seed_fails_if_float():
+    """Test that the seed property fails with a float."""
+    m = PolynomialChaos()
+    seed = 42.0
+    m.seed = seed
+
+
+def test_str_special():
+    """Test type of __str__ method results."""
+    s = str(x)
+    assert_true(type(s) is str)
+
+
+def test_str_length():
+    """Test the default length of __str__."""
+    s = str(x)
+    n_lines = len(s.splitlines())
+    assert_equal(n_lines, 7)
