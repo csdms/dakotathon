a91cb126b65cf77837d0937a03d41735b5cfef28
Mark Piper <mark.piper@colorado.edu>
2016-07-29 16:19:20 -0600

    Subclass Sampling from UncertaintyQuantificationBase

--- csdms/dakota/method/sampling.py
+++ csdms/dakota/method/sampling.py
@@ -1,31 +1,18 @@
 #! /usr/bin/env python
 """Implementation of the Dakota sampling method."""
 
-from .base import MethodBase
+from .base import UncertaintyQuantificationBase
 
 
 classname = 'Sampling'
 
 
-class Sampling(MethodBase):
+class Sampling(UncertaintyQuantificationBase):
 
-    """Define parameters for a Dakota experiment using the sampling method."""
+    """The Dakota sampling method."""
 
-    def __init__(self,
-                 samples=10,
-                 sample_type='random',
-                 seed=None,
-                 **kwargs):
+    def __init__(self, **kwargs):
         """Create a new Dakota sampling study.
-
-        Parameters
-        ----------
-        samples: int
-          The number of randomly chosen values at which to execute a model.
-        sample_type: str
-          Technique for choosing samples, `random` or `lhs`.
-        seed: int
-          The seed for the random number generator.
 
         Examples
         --------
@@ -34,81 +21,17 @@ class Sampling(MethodBase):
         >>> x = Sampling()
 
         """
-        MethodBase.__init__(self, **kwargs)
+        UncertaintyQuantificationBase.__init__(self, **kwargs)
         self.method = self.__module__.rsplit('.')[-1]
-        self._samples = samples
-        self._sample_type = sample_type
-        self._seed = seed
-
-    @property
-    def samples(self):
-        """Number of samples in experiment."""
-        return self._samples
-
-    @samples.setter
-    def samples(self, value):
-        """Set number of samples used in experiment.
-
-        Parameters
-        ----------
-        value : int
-        The number of samples.
-
-        """
-        if type(value) is not int:
-            raise TypeError("Samples must be an int")
-        self._samples = value
-
-    @property
-    def sample_type(self):
-        """Sampling strategy."""
-        return self._sample_type
-
-    @sample_type.setter
-    def sample_type(self, value):
-        """Set sampling strategy used in experiment.
-
-        Parameters
-        ----------
-        value : str
-        The sampling strategy.
-
-        """
-        if ['random', 'lhs'].count(value) == 0:
-            raise TypeError("Sample type must be 'random' or 'lhs'")
-        self._sample_type = value
-
-    @property
-    def seed(self):
-        """Seed of the random number generator."""
-        return self._seed
-
-    @seed.setter
-    def seed(self, value):
-        """Set the seed of the random number generator.
-
-        Parameters
-        ----------
-        value : int
-        The random number generator seed.
-
-        """
-        if type(value) is not int:
-            raise TypeError("Seed must be an int")
-        self._seed = value
 
     def __str__(self):
         """Define the method block for a sampling experiment.
 
         See Also
         --------
-        csdms.dakota.method.base.MethodBase.__str__
+        csdms.dakota.method.base.UncertaintyQuantificationBase.__str__
 
         """
-        s = MethodBase.__str__(self)
-        s += '    sample_type = {}\n'.format(self.sample_type) \
-            + '    samples = {}\n'.format(self.samples)
-        if self.seed is not None:
-            s += '    seed = {}\n'.format(self.seed)
+        s = UncertaintyQuantificationBase.__str__(self)
         s += '\n'
         return(s)
--- csdms/dakota/tests/test_method_sampling.py
+++ csdms/dakota/tests/test_method_sampling.py
@@ -39,66 +39,6 @@ def test_method_attr():
     assert_equal(x.method, 'sampling')
 
 
-def test_get_samples():
-    """Test getting the samples property."""
-    assert_true(type(x.samples) is int)
-
-
-def test_set_samples():
-    """Test setting the samples property."""
-    samples = 42
-    x.samples = samples
-    assert_equal(x.samples, samples)
-
-
-@raises(TypeError)
-def test_set_samples_fails_if_float():
-    """Test that the samples property fails with a float."""
-    samples = 42.0
-    x.samples = samples
-
-
-def test_get_sample_type():
-    """Test getting the sample_type property."""
-    assert_true(x.sample_type == 'random' or x.sample_type == 'lhs')
-
-
-def test_set_sample_type():
-    """Test setting the sample_type property."""
-    sample_type = 'lhs'
-    x.sample_type = sample_type
-    assert_equal(x.sample_type, sample_type)
-
-
-@raises(TypeError)
-def test_set_sample_type_fails_if_not_lhs_or_random():
-    """Test that the sample_type property fails with unknown type."""
-    sample_type = 'mcmc'
-    x.sample_type = sample_type
-
-
-def test_get_seed():
-    """Test getting the seed property."""
-    if x.seed is not None:
-        assert_true(type(x.seed) is int)
-    else:
-        assert_is_none(x.seed)
-
-
-def test_set_seed():
-    """Test setting the seed property."""
-    seed = 42
-    x.seed = seed
-    assert_equal(x.seed, seed)
-
-
-@raises(TypeError)
-def test_set_seed_fails_if_float():
-    """Test that the seed property fails with a float."""
-    seed = 42.0
-    x.seed = seed
-
-
 def test_str_special():
     """Test type of __str__ method results."""
     s = str(x)
@@ -107,7 +47,6 @@ def test_str_special():
 
 def test_str_length():
     """Test the default length of __str__."""
-    x1 = Sampling()
-    s = str(x1)
+    s = str(x)
     n_lines = len(s.splitlines())
     assert_equal(n_lines, 5)
