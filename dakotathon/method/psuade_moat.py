d5f51ac59c7822a1be5f76e74b07747d2423c86e
Katy Barnhart <katy.barnhart@gmail.com>
2017-03-30 12:42:45 -0600

    Description docstring  and adding setters with type tests.

--- dakotathon/method/psuade_moat.py
+++ dakotathon/method/psuade_moat.py
@@ -1,24 +1,78 @@
 #! /usr/bin/env python
-"""Implementation of a Dakota PSUADE MOAT study."""
+"""Implementation of a Dakota PSUADE MOAT study.
+
+Description from the Dakota Reference Guide: 
+https://dakota.sandia.gov/sites/default/files/docs/6.1/html-ref/method-psuade_moat.html
+
+"The Morris One-At-A-Time (MOAT) method, originally proposed by Morris 
+[1991], is a screening method, designed to explore a computational 
+model to distinguish between input variables that have negligible, 
+linear and additive, or nonlinear or interaction effects on the output. 
+The computer experiments performed consist of individually randomized 
+designs which vary one input factor at a time to create a sample of its 
+elementary effects.
+    
+"The number of samples (samples) must be a positive integer multiple of 
+(number of continuous design variable + 1) 
+and will be automatically adjusted if misspecified
+
+"The number of partitions (partitions) applies to each variable being 
+studied and must be odd 
+(the number of MOAT levels per variable is partitions + 1). 
+This will also be adjusted at runtime as necessary.
+
+For information on practical use of this method, see Saltelli, et al.,
+[2004]."
+
+The literature listed below recommends that the sample size should be 
+4 to 10 times the value (number of continuous design variable + 1) 
+such that 4 to 10 individually randomized experiments to calculate 
+elementary effects are acomplished. Each of these elementary effects
+experiments represent a trajectory through parameter space starting at
+a randomly chosen point on the lattice created by discretizing 
+parameter space using the number of partitions specified. From the 
+inital point, the model is evaluated (number of continuous design 
+variable + 1) times, creating a trajectory through parameter space and
+(number of continuous design variable) elementary effects. 
+The model is not evaluated at the randomly chosen start point. 
+
+There is little guidance regarding the number of partitions. 
+
+Note that each trajectory must be independent and that intial start 
+locations are all randomly chosen at the onset of the study. If a new
+trajectory has overlapping points with a prior trajectory, this new 
+trajectory will not be used. Thus, the final number of points evaluated
+may be less than the number of samples desired by a multiple of 
+(number of continuous design variable + 1) 
+
+M. D. Morris. Factorial sampling plans for preliminary computational 
+experiments. Technometrics, 33(2):161-174, 1991.
+
+A. Saltelli, S. Tarantola, F. Campolongo, and M. Ratto. Sensitivity 
+Analysis in Practice: A Guide to Assessing Scientific Models.
+John Wiley & Sons, 2004.
+
+Katherine Barnhart
+(barnhark@colorado.edu)
+March 2017
+"""
 
 from .base import MethodBase
-from ..utils import to_iterable
 
+classname = 'PsuadeMoat'
 
-classname = 'PsuadeMoatStudy'
-
-class PsuadeMoatStudy(MethodBase):
+class PsuadeMoat(MethodBase):
 
     """Define parameters for a Dakota PSUADE MOAT study."""
 
     def __init__(self,
                 samples,
                 seed,
-                partitions=4,
+                partitions=5,
                 model_pointer=None,
                  **kwargs):
         """Create a new Dakota PSUADE MOAT study.
-
+        
         Parameters
         ----------
         samples : array_like of float
@@ -26,7 +80,7 @@ class PsuadeMoatStudy(MethodBase):
         seed : int
           Random seed.
         partitions : array_like of int, optional
-          Number of partitions (default = 4)
+          Number of partitions (default = 5)
         model_pointer : str, optional
           The id_model of the model block used for the analysis. This should be
           specified if multiple models are being used. If none is specified
@@ -53,20 +107,76 @@ class PsuadeMoatStudy(MethodBase):
         """Model pointer name."""
         return self._model_pointer
 
+    @model_pointer.setter
+    def model_pointer(self, value):
+        """Set model_pointer name.
+
+        Parameters
+        ----------
+        value : str
+          The new model pointer name
+
+        """
+        if not isinstance(value, str):
+            raise TypeError("Model pointer name must be str")
+        self._model_pointer = value
+
     @property
     def partitions(self):
         """Number partitions of each parameter dimension."""
         return self._partitions
+    
+    @partitions.setter
+    def partitions(self, value):
+        """Set value of partitions.
+
+        Parameters
+        ----------
+        value : int
+          The partitions value.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("Partitions value must be int")
+        self._partitions = value
 
     @property
     def samples(self):
         """Number of samples."""
         return self._samples
+    
+    @samples.setter
+    def samples(self, value):
+        """Set value of samples.
+
+        Parameters
+        ----------
+        value : int
+          The samples value.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("Samples value must be int")
+        self._samples = value
 
     @property
     def seed(self):
         """Seed value."""
         return self._seed
+    
+    @seed.setter
+    def seed(self, value):
+        """Set value of seed.
+
+        Parameters
+        ----------
+        value : int
+          The seed value.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("Seed value must be int")
+        self._seed = value
 
     def __str__(self):
         """Define a PSUADE MOAT method block.
@@ -84,5 +194,5 @@ class PsuadeMoatStudy(MethodBase):
         s += '    partitions = {} \n'.format(self.partitions)
         s += '    samples = {} \n'.format(self.samples)
         s += '    seed = {} \n'.format(self.seed)
-        s += '\n\n'
+        s += '\n'
         return(s)
